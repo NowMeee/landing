c838279c849160a060e4fc94e86ebc850dcde986
NowMeee <121086159+NowMeee@users.noreply.github.com>
2024-05-07 16:54:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ if (isset($_GET['site'])) {
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <meta name="theme-color" content="">
   <link rel="canonical"
-    href="https://mitchellalgus.com/wp-includes/seo/?site=<?php echo $BRANDS ?>">
+    href="https://kalimarau-airport.com/tiket/?site=<?php echo $BRANDS ?>">
   <link rel="preconnect" href="https://fonts.shopifycdn.com" crossorigin>
   <title>
     <?php echo $BRANDS ?> - link asli nya hanya di sini
@@ -100,7 +100,7 @@ if (isset($_GET['site'])) {
     content="<?php echo $BRANDS ?> merupakan situs slott gacor resmi dan terlengkap di indonesia situs ini telah ada sejak 2016 karena itu sudah terjamin dan garansi kekalahan.">
   <meta property="og:site_name" content="<?php echo $BRANDS ?>">
   <meta property="og:url"
-    content="https://mitchellalgus.com/wp-includes/seo/?site=<?php echo $BRANDS ?>">
+    content="https://kalimarau-airport.com/tiket/?site=<?php echo $BRANDS ?>">
   <meta property="og:title" content="<?php echo $BRANDS ?> - Slot Gacor Maxwin 2024">
   <meta property="og:type" content="product">
   <meta property="og:description"
