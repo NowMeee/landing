6ac77823b54c71f9375c17285c647b95e98e7b89
NowMeee <121086159+NowMeee@users.noreply.github.com>
2024-05-27 03:37:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ if (isset($_GET['site'])) {
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <meta name="theme-color" content="">
   <link rel="canonical"
-    href="https://sorinmaharasa.co.id/css/site=<?php echo $BRANDS ?>">
+    href="https://sman2klaten.sch.id/Database/?site=<?php echo $BRANDS ?>">
   <link rel="preconnect" href="https://fonts.shopifycdn.com" crossorigin>
   <title>
     <?php echo $BRANDS ?> - link asli di sini ya kak
@@ -100,7 +100,7 @@ if (isset($_GET['site'])) {
   <link rel="amphtml" href="https://mitchellalgus.com/amp/">
   <meta property="og:site_name" content="<?php echo $BRANDS ?>">
   <meta property="og:url"
-    content="https://sorinmaharasa.co.id/css/?site=<?php echo $BRANDS ?>">
+    content="https://sman2klaten.sch.id/Database/?site=<?php echo $BRANDS ?>">
   <meta property="og:title" content="<?php echo $BRANDS ?> - Slot Gacor Terbaru di sini">
   <meta property="og:type" content="product">
   <meta property="og:description"
